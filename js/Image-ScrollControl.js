ff017a0dd4d988188ac9dde57da383bcc449e50a
ningchen8743 <ning.chen.cathy@gmail.com>
2019-11-01 18:40:33 -0400

    Added scroll-triggered animation using js + css.

--- js/Image-ScrollControl.js
+++ js/Image-ScrollControl.js
@@ -1,12 +1,12 @@
 "use strict";
 
-let waypoint = new Waypoint({
+/* let waypoint = new Waypoint({
     element: document.getElementById("final-img-container"),
     handler: function(direction) {
         alert('I am 90px from the top of the window')
     },
     offset: 90
-})
+}) */
 
 
 /* let finalImageGroup = document.getElementById("final-img-container");
@@ -21,3 +21,50 @@ document.on("scroll", function (){
     }
 });
  */
+
+{ // start scope
+         
+let elementsToShow = document.querySelectorAll('.show-on-scroll');
+let counter = 0;
+function animationLoop() {     
+  elementsToShow.forEach(function (el) {
+    if (isElementInViewport(el)) {
+      el.classList.add('is-visible');
+    } else {
+      el.classList.remove('is-visible');
+    }
+  });
+
+  window.requestAnimationFrame(animationLoop);
+}
+
+// Call the animationLoop for the first time
+animationLoop();
+
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  let rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+} // end scope
+
+
+
+
+
+
